0a84a371b7a166210672a8c380608331d4e16b15
shmohamud <shmohamud@email.wm.edu>
2019-10-14 18:44:03 -0400

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,76 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Paintball from "./components/Paintball/Paintball";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Target from "./components/Target/Target";
 import Timer from "./components/Timer /Timer";
 
-class App extends React.Component {
-  state = {
-    ammunition: "O O O O",
-    secondsRemaining: 15,
-    x: 0,
-    y: 0,
-    shotCoordinates: [],
-    targetCoordinates: ["20em", "35em"],
-    numShots: 0,
-    points: 0,
-  };
+function App() {
+  const [ammunition, setAmmo] = useState("O O O O");
+  const [secondsRemaining, setSeconds] = useState(15);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  let [shotCount, setCount] = useState(0);
+  const [points, setPoints] = useState(0);
+  const [shotCoordinates, setShotCoords] = useState([]);
+  const [targetCoordinates, setTargetCoords] = useState(["20em", "35em"]);
 
-  componentDidMount() {
-    window.addEventListener("click", e => this.onShootPaintball(e));
-  }
+  useEffect(() => {
+    window.addEventListener("click", e => onShootPaintball(e));
+  });
 
-  onShootPaintball = e => {
+  const onShootPaintball = e => {
     let { clientX, clientY } = e;
-    let state = { ...this.state };
-    this.setState({
-      ...state,
-      ammunition: state.ammunition
+
+    setAmmo(
+      ammunition
         .split(" ")
         .slice(1)
-        .join(" "),
-      x: clientX,
-      y: clientY,
-      numShots: (state.numShots += 1),
-      shotCoordinates: [
-        ...state.shotCoordinates.slice(),
-        [clientX, clientY, this.setPaintColor()]
-      ]
-    });
-    this.destroyPaintball();
+        .join(" ")
+    );
+
+    setX(clientX);
+    setY(clientY);
+    const newCoordsAndColor = [clientX, clientY, setPaintColor()];
+    setCount((shotCount += 1));
+    const newShotCoords = [...shotCoordinates.slice(), ...newCoordsAndColor];
+    setShotCoords(newShotCoords);
+    destroyPaintball();
   };
 
-    destroyPaintball = () => {
+  const destroyPaintball = () => {
     setTimeout(() => {
-      const state = { ...this.state };
-      const shotCoordinates = state.shotCoordinates.slice(1);
-      this.setState({
-        ...state,
-        shotCoordinates: shotCoordinates
-      });
+      setShotCoords(shotCoordinates.slice(1));
     }, 5000);
   };
 
-  onTargetHit = () => {
-    const {top, right} = this.getRandomCoordinates()
-    const state = { ...this.state };
-    const points = state.points + 10;
-    this.setState({
-      targetCoordinates: [top, right],
-      points: points,
-      shotCoordinates: []
-    });
-  }
+  const onTargetHit = () => {
+    const { top, right } = getRandomCoordinates();
+    setPoints(points + 10);
+    setTargetCoords([top, right]);
+  };
 
-  getRandomCoordinates = () => {
+  const getRandomCoordinates = () => {
     const max = 35;
     const min = 5;
-    const randNumTop = Math.floor(Math.random() * (max - min)).toString() + "em";
-    const randNumRight = Math.floor(Math.random() * (max - min)).toString() + "em";
-      return {top: randNumTop, right:randNumRight}
-  }
+    const randNumTop =
+      Math.floor(Math.random() * (max - min)).toString() + "em";
+    const randNumRight =
+      Math.floor(Math.random() * (max - min)).toString() + "em";
+    return { top: randNumTop, right: randNumRight };
+  };
 
-  setPaintColor = () => {
+  const setPaintColor = () => {
     const colors = ["blue", "black", "green", "orange", "purple", "red"];
     const min = 0;
     const max = 5;
@@ -78,59 +67,36 @@ class App extends React.Component {
     return colors[randNum];
   };
 
-  decrementSeconds = () => {
-    if (this.state.secondsRemaining) {
+  const decrementSeconds = () => {
+    if (secondsRemaining) {
       setTimeout(() => {
-        let { secondsRemaining } = this.state;
-        console.log(this.state.seconds, this.state);
-        this.setState(() => ({ secondsRemaining: secondsRemaining - 1 }));
+        setSeconds(() => secondsRemaining - 1);
       }, 1000);
     }
   };
 
-  handleClickResetGame = () => {
-    const state = { ...this.state };
-    this.setState({
-      ...state,
-      ammunition: "O O O O O",
-      numShots: 0,
-      points: 0,
-      shotCoordinates: [],
-      secondsRemaining: 15,
-    });
+  const handleClickResetGame = () => {
+    alert("Reset Game!");
   };
-
-
-  render() {
-    return (
-      <div>
-        {this.state.shotCoordinates.map(shotCoords => (
-          <Paintball
-            x={shotCoords[0]}
-            y={shotCoords[1]}
-            color={shotCoords[2]}
-          />
-        ))}
-        <Target
-          top={this.state.targetCoordinates[0]}
-          right={this.state.targetCoordinates[1]}
-          targetHit={this.onTargetHit}
-        />
-        <Timer
-          decrementSeconds={this.decrementSeconds}
-          seconds={this.state.secondsRemaining}
-          levelComplete={this.state.levelComplete}
-          fail={this.state.levelFailure}
-        />
-        <Sidebar
-          ammo={this.state.ammunition}
-          numShots={this.state.numShots}
-          points={this.state.points}
-          resetGame={this.handleClickResetGame}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      {shotCoordinates.map(shotCoords => (
+        <Paintball x={shotCoords[0]} y={shotCoords[1]} color={shotCoords[2]} />
+      ))}
+      <Target
+        top={targetCoordinates[0]}
+        right={targetCoordinates[1]}
+        targetHit={onTargetHit}
+      />
+      <Timer decrementSeconds={decrementSeconds} seconds={secondsRemaining} />
+      <Sidebar
+        ammo={ammunition}
+        shotCount={shotCount}
+        points={points}
+        resetGame={handleClickResetGame}
+      />
+    </div>
+  );
 }
 
 export default App;
